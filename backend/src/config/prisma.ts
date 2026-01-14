861c037c6c2898a484849b316544ab3d339ef5c9
adhinov <adhinov4@gmail.com>
2026-01-14 20:44:47 +0700

    perubahan mekanise error login

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,18 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma =
-  global.prisma ??
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error"],
+    log: ['error', 'warn'],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
